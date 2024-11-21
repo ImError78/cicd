14e5d05396bac496a066940c065b8fd601b5c077
ImError78 <atheonas@teka.vionet.gr>
2024-11-21 13:17:26 +0200

    added basic server

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
+
 const { exec } = require('child_process');
+
+const http = require('http')
+const port = 8080
+
+var hello = (_, res) => {
+    res
+        .write('Hello World')
+        .end()
+}
+
+var check = (error) => {
+    if (error) {
+        console.log('Something went wrong', error);
+    }
+    else {
+        console.log('Server is listening on port' + port);
+    }
+}
 
 var cb = (e, o, oe) => {
     if (e) { console.log(e) }
@@ -11,3 +30,7 @@ var test_sh = ['pwd','uname -r','lsb_release -a']
 
 test_sh.forEach(_)
 
+// Create a server object:
+http
+    .createServer(hello)
+    .listen(port, check)
